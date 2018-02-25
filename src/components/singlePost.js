43e11bbb9157bdaf8e5db0ad8e8eaf9784c72059
Aria Vaghef <aria.vaghef@gmail.com>
2018-02-25 12:11:34 -0800

    singlePost passes data faster now

--- src/components/singlePost.js
+++ src/components/singlePost.js
@@ -6,20 +6,41 @@ import FullPost from './FullPost'
 
 
 class singlePost extends Component {
+  constructor(props) {
+    super(props);
+    
+    const testPost = props.posts.filter((post) => post.id === props.match.params.postId)
+   
+    if(testPost.length === 0){
+      this.props.getpost(this.props.match.params.postId)
+    }
+    else{
+      this.state = {
+          post: testPost[0]}
+
+      }
+    }
   componentDidMount(){
-    this.props.getpost(this.props.match.params.postId)
+    
   }
   render() {
     //this.props.match.params.category ? this.props.setCat(this.props.match.params.category) : this.props.setCat('All')
     const { posts } = this.props
-    const post = posts[0] || {}
+    var tempPost = {}
+    if(this.state){
+      tempPost = Object.assign({}, this.state.post)
+      
+    }
+    else{
+      tempPost = posts[0] || {}
+    }
     
     return (
     <div>
       <div className="sidebar">
           <CategorySidebar/>
       </div>
-          <FullPost post={post}/>
+      {((tempPost) && (tempPost.id === this.props.match.params.postId)) && <FullPost post={tempPost}/>}
 
     </div>
 
